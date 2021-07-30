322abbc7d144fabf44f3897a149887acab6cc64a
botjac <botha_jacques@yahoo.com>
2021-07-30 16:00:27 +0200

    Amendments to course materials

---
+++ src/test/java/HelloWorldJunit5Test.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloWorldJunit5Test {
+
+    @Test
+    void getHello() {
+        System.out.println("I ran...");
+    }
+}
